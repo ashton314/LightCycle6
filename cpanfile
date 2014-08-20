52a5bf5c44fcfbf85896e28b3213137755836b60
Ashton Wiersdorf <ashton.wiersdorf@mailblock.net>
2014-08-19 21:43:23 -0600

    - installed modules

---
+++ cpanfile
@@ -0,0 +1,3 @@
+requires 'AnyEvent', '7.07';
+requires 'EV', '4.17';
+requires 'Quantum::Superpositions', '2.02';
---
+++ grid.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# core modules
+use Data::Dumper;
+
+# third-party libs
+use AnyEvent;
+
+# my modules
+use Cycle::Human;
+use Cycle::Computer;
